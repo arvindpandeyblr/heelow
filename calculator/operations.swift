870a99db2bbad86bb30729fa839748130ceff16e
Arvind Pandey <arvind.pandey@bcone.com>
2021-04-09 14:26:44 +0530

    calculator

---
+++ calculator/operations.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class operations {
+    
+    func Addition(_ number1:Int, number2:Int)->Int {
+        let Result = number1 + number2
+        return Result
+        
+    }
+
+    func Subtraction(_ number1:Int, number2:Int)->Int {
+        let Result = number1 - number2
+        return Result
+    }
+
+
+}
+
